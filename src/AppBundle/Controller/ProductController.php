bc7483b215a42ee5a0924c751867bdcf0d9754df
apch <apch_str@yahoo.com>
2017-04-10 02:31:38 +0300

    edit, delete product functionality, edit image, file upload in service

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -7,6 +7,7 @@ use AppBundle\Form\ProductType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,10 +16,20 @@ class ProductController extends Controller
 {
     /**
      * @Route("/products", name="all_products")
+     *
+     * @return Response
      */
     public function viewAll()
     {
-        return new Response();
+        $products = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findBy([], ['price' => 'asc', 'name' => 'asc']);
+
+        return $this->render('products/view_all.html.twig',
+            [
+                'products' => $products
+            ]
+        );
     }
 
     /**
@@ -57,18 +68,8 @@ class ProductController extends Controller
              * @var UploadedFile $file
              */
             $file = $product->getImageUrl();
+            $fileName = $this->get('app.product_image_uploader')->upload($file);
 
-            // Generate a unique name for the file before saving it
-            $fileName = md5(uniqid()).'.'.$file->guessExtension();
-
-            // Move the file to the directory where brochures are stored
-            $file->move(
-                $this->getParameter('products_images_directory'),
-                $fileName
-            );
-
-            // Update the 'brochure' property to store the PDF file name
-            // instead of its contents
             $product->setImageUrl($fileName);
 
             $em = $this->getDoctrine()->getManager();
@@ -77,7 +78,7 @@ class ProductController extends Controller
 
             $this->addFlash("success", "Product with name ". $product->getName() . " was added successfully");
 
-            return $this->redirectToRoute("add_product_form");
+            return $this->redirectToRoute("all_products");
         }
 
         return $this->render("products/add.html.twig",
@@ -85,5 +86,84 @@ class ProductController extends Controller
                 'productForm' => $form->createView()
             ]
         );
+    }
+
+    /**
+     * @Route("/products/edit/{id}", name="edit_product_form")
+     * @Method("GET")
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function edit(Product $product)
+    {
+        $product->setImageUrl(
+            new File($this->getParameter('products_images_directory').'/'.$product->getImageUrl())
+        );
+        echo $product->getImageUrl();
+        $form = $this->createForm(ProductType::class, $product);
+
+        return $this->render("products/edit.html.twig",
+            ['productForm' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/products/edit/{id}", name="edit_product_process")
+     * @Method("POST")
+     *
+     * @param Product $product
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function editProcess(Product $product, Request $request)
+    {
+        $product->setImageUrl(
+            new File($this->getParameter('products_images_directory').'/'.$product->getImageUrl())
+        );
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+
+            // $file stores the uploaded PDF file
+            /**
+             * @var UploadedFile $file
+             */
+            $file = $product->getImageUrl();
+            $fileName = $this->get('app.product_image_uploader')->upload($file);
+
+            $product->setImageUrl($fileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash("success", "Product with name ". $product->getName() . " was edited successfully");
+
+            return $this->redirectToRoute("all_products");
+        }
+
+        return $this->render("products/add.html.twig",
+            [
+                'productForm' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/products/delete/{id}", name="delete_product_process")
+     * @Method("POST")
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function delete(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+        $this->addFlash("delete", "Product deleted!!!");
+        return $this->redirectToRoute("all_products");
     }
 }
